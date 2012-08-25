6c4b747fcdf957fd879d8af05254bc3a9016a4b7
Christian Bühler <christian@cbuehler.de>
2012-08-26 00:37:27 +0200

    embedInto

--- ocm-marble/main.cpp
+++ ocm-marble/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     QX11EmbedWidget w;
     Marble::MarbleWidget m(&w);
+    if (a.arguments().count() > 1)
+        w.embedInto(a.arguments()[1].toULong());
     w.show();
     
     return a.exec();
